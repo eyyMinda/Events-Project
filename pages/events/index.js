5ccb10142928c7b20647952ba1dfa487de5416c8
eyyMinda <gamerx14x@gmail.com>
2023-08-11 02:04:52 +0300

    Routes setup

---
+++ pages/events/index.js
@@ -0,0 +1,8 @@
+
+export default function EventsPage() {
+  return (
+    <div>
+      <h1>Events Page Template</h1>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import Link from "next/link";
 
 export default function HomePage() {
+  const specificRoutes = [
+    { id: 'comicon', name: 'ComiCon' }
+  ]
+  const pathname = '/events/'
+
   return (
     <div>
       <h1>Events Home Page</h1>
+
+      <ul>
+        <li>
+          <Link href={{
+            pathname
+          }}>Events</Link>
+        </li>
+        <li>
+          <Link href={{
+            pathname: pathname + '[id]',
+            query: { id: specificRoutes[0].id }
+          }}>Event Details Page ({specificRoutes[0].name})</Link>
+        </li>
+        <li>
+          <Link href={{
+            pathname: pathname + '[year]/' + '[month]',
+            query: { year: '2023', month: '8' }
+          }}>Filtered Events Page</Link>
+        </li>
+      </ul>
     </div>
   )
 }
