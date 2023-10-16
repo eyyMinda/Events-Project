4d1f4d0667f0ef57f3046d8b6ffeb4061975ed5e
eyyMinda <gamerx14x@gmail.com>
2023-10-16 13:23:05 +0300

    env

--- next.config.js
+++ next.config.js
@@ -6,9 +6,9 @@ const nextConfig = {
     defaultLocale: 'en',
   },
   // output: "export",
-  env: {
-    DB_URL: "https://wwafeww-e792f-default-rtdb.europe-west1.firebasedatabase.app/"
-  }
+  // env: {
+  //   DB_URL: "https://wwafeww-e792f-default-rtdb.europe-west1.firebasedatabase.app/"
+  // }
 };
 
 module.exports = nextConfig;
