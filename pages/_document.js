5b2c3f13239abdfb3bdcd319acf7a15cfdbd4c6a
eyyMinda <gamerx14x@gmail.com>
2023-10-16 23:04:36 +0300

    _document.js

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <div id="overlays" />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+// For showcase, lang="en" is also in next.config.js
