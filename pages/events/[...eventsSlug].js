8ed4855cb6fad39832913d776f1b396eea1ae72b
eyyMinda <gamerx14x@gmail.com>
2023-10-16 12:50:07 +0300

    replaced ServerSideProps with client fetching (useSWR)

--- pages/events/[...eventsSlug].js
+++ pages/events/[...eventsSlug].js
@@ -1,25 +1,46 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { getFilteredEvents } from "@/helpers/api-util";
+import useSWR from "swr";
+import { transformObjToArr } from "@/helpers/utility";
 
 import ErrorAlert from "@/components/events/error-alert/error-alert";
 import Button from "@/components/ui/button";
 import EventList from "@/components/events/event-list/eventList";
 import ResultsTitle from "@/components/events/events-search/results-title";
+const api = process.env.DB_URL + "events.json";
+const fetcher = (...args) =>
+  fetch(...args)
+    .then(res => res.json())
+    .then(data => transformObjToArr(data));
 
-export default function FilteredEventsPage(props) {
-  const router = useRouter();
-  const dataFilter = router.query.eventsSlug;
+export default function FilteredEventsPage() {
+  const [events, setEvents] = useState();
+  const dataFilter = useRouter().query.eventsSlug;
+
+  const { data, err, isLoading } = useSWR(api, fetcher);
+  useEffect(() => data && setEvents(data), [data]);
+
   // Loading Filter Route
-  if (!dataFilter) {
+  if (!events || isLoading) {
     return <ErrorAlert>Loading...</ErrorAlert>;
   }
+  const [year, month] = dataFilter.map(Number);
 
-  if (props.hasError) {
+  // Check if valid Filter
+  if (
+    isNaN(year) ||
+    isNaN(month) ||
+    year > 2030 ||
+    year < 2021 ||
+    month < 1 ||
+    month > 12 ||
+    err
+  ) {
     return (
       <Fragment>
         <ErrorAlert>
           <p>Invalid Filter. Please adjust your values!</p>
+          {err && <description>{err}</description>}
         </ErrorAlert>
         <div className="center">
           <Button link="/events/">Browse All Events</Button>
@@ -28,10 +49,13 @@ export default function FilteredEventsPage(props) {
     );
   }
 
-  const { events, date } = props;
+  const filteredEvents = events.filter(e => {
+    const eDate = new Date(e.date);
+    return eDate.getFullYear() === year && eDate.getMonth() === month - 1;
+  });
 
   // Check if Events exist based on Filter
-  if (!events[0]) {
+  if (!filteredEvents || !filteredEvents[0]) {
     return (
       <Fragment>
         <ErrorAlert>
@@ -43,45 +67,35 @@ export default function FilteredEventsPage(props) {
       </Fragment>
     );
   } else {
-    const currentDate = new Date(date.year, date.month - 1);
+    const currentDate = new Date(year, month - 1);
 
     return (
       <Fragment>
         <ResultsTitle date={currentDate} />
-        <EventList events={events} />
+        <EventList events={filteredEvents} />
       </Fragment>
     );
   }
 }
 
-export async function getServerSideProps(context) {
-  const { params } = context;
-  const [year, month] = params.eventsSlug.map(Number);
+// Server-Side Rendering for prerendering the pages (cannot work with client fetching simultaneously)
+// export async function getServerSideProps(context) {
+//   const { params } = context;
+//   const [year, month] = params.eventsSlug.map(Number);
 
-  if (
-    isNaN(year) ||
-    isNaN(month) ||
-    year > 2030 ||
-    year < 2021 ||
-    month < 1 ||
-    month > 12
-  ) {
-    return {
-      props: { hasError: true }
-    }
-  }
-  const events = await getFilteredEvents({ year, month });
+//   if (
+//     isNaN(year) ||
+//     isNaN(month) ||
+//     year > 2030 ||
+//     year < 2021 ||
+//     month < 1 ||
+//     month > 12
+//   ) {
+//     return {
+//       props: { hasError: true }
+//     }
+//   }
+//   const events = await getFilteredEvents({ year, month });
 
-  return { props: { events, date: { year, month } } };
-}
-
-// export async function getStaticPaths() {
-//   const events = await getFeaturedEvents();
-//   const paths = events.map(event => {
-//     const [year, month] = event.date.split('-');
-
-//     return { params: { eventsSlug: [year, month] } };
-//   });
-
-//   return { paths, fallback: true };
+//   return { props: { events, date: { year, month } } };
 // }
