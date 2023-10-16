49d530c864030f20b6b48ac913e06b4a5bd9ab34
eyyMinda <gamerx14x@gmail.com>
2023-10-16 13:19:23 +0300

    lighthouse optimizations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
   // output: "export",
   env: {
     DB_URL: "https://wwafeww-e792f-default-rtdb.europe-west1.firebasedatabase.app/"
