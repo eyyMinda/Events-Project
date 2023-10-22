c279fcb90b7a9a7ab652be913deece31dedc556f
eyyMinda <gamerx14x@gmail.com>
2023-10-22 22:18:51 +0300

    compacted events fetching funcs

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -1,33 +1,17 @@
 import { transformObjToArr } from "./utility";
 export const api = process.env.NEXT_PUBLIC_DB_URL;
 
-export async function getAllEvents() {
-  const response = await fetch(api + "events.json");
-  const data = await response.json();
-  return transformObjToArr(data);
-}
+//================== EVENTS ====================
 
+export const getAllEvents = async () => transformObjToArr(await (await fetch(api + "events.json")).json());
 
-export async function getFeaturedEvents() {
-  const allEvents = await getAllEvents();
-  return allEvents.filter(event => event.isFeatured);
-}
+export const getFeaturedEvents = async () => (await getAllEvents()).filter(event => event.isFeatured);
 
+export const getEventById = async id => (await getAllEvents()).find(event => event.id === id);
 
-export async function getEventById(id) {
-  const allEvents = await getAllEvents();
-  return allEvents.find(event => event.id === id);
-}
-
-
-export async function getFilteredEvents(dateFilter) {
-  const allEvents = await getAllEvents();
-  const { year, month } = dateFilter;
-
-  return allEvents.filter(event => {
+export const getFilteredEvents = async ({ year, month }) => {
+  return (await getAllEvents()).filter(event => {
     const eventDate = new Date(event.date);
-    return (
-      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
-    );
+    return eventDate.getFullYear() === year && eventDate.getMonth() === month - 1;
   });
-}
+};
