8c25ce8bdab6a0b171c9d20185b8e31b6c3cd7d2
eyyMinda <gamerx14x@gmail.com>
2023-10-24 23:01:01 +0300

    mongodb newsletter

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,7 @@
 import { isValid } from "@/helpers/authValidation";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const mongoUri = process.env.NEXT_PUBLIC_MONGO_URL;
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
@@ -9,6 +12,23 @@ export default function handler(req, res) {
       res.status(422).json({ err: emailErr, msg: emailMsg });
       return;
     }
+
+    const client = new MongoClient(mongoUri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+      }
+    });
+    async function run() {
+      try {
+        await client.connect();
+        const db = client.db("events_nextjs");
+        await db.collection("newsletter_emails").insertOne({ email });
+      } finally { await client.close(); }
+    }
+    run().catch(console.dir);
+
 
     res.status(201).json({ err: emailErr, msg: "You've succesfully signed-up to receive our newsletter!" });
   }
