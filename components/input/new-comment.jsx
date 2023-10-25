1913ad3767140f34b6a6ee9ab60306f7f2fbbaa6
eyyMinda <gamerx14x@gmail.com>
2023-10-26 02:51:55 +0300

    comment state

--- components/input/new-comment.jsx
+++ components/input/new-comment.jsx
@@ -1,10 +1,20 @@
 import css from "./styles/new-comment.module.css";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function NewComment({ onAddComment, inputRes }) {
+  const [commentState, setCommentState] = useState(false);
   const emailInputRef = useRef();
   const nameInputRef = useRef();
   const commentInputRef = useRef();
+
+  function handleCommentFocus(e) {
+    e.target.placeholder = "";
+    !commentState && setCommentState(true);
+  }
+  function handleCancelFocus() {
+    commentInputRef.current.placeholder = "Add a comment...";
+    commentState && setCommentState(false);
+  }
 
   function handleSendComment(e) {
     e.preventDefault();
@@ -13,29 +23,46 @@ export default function NewComment({ onAddComment, inputRes }) {
       name: nameInputRef.current.value,
       text: commentInputRef.current.value,
     };
-
     onAddComment(input);
   }
 
   return (
     <form className={css.form} onSubmit={handleSendComment}>
-      <div className={css.row}>
-        <div className={css.control}>
-          <label htmlFor="email">Your email</label>
-          <input type="email" id="email" ref={emailInputRef} />
+      {commentState && (
+        <div className={css.row}>
+          <div className={css.control}>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" ref={emailInputRef} />
+          </div>
+          <div className={css.control}>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" ref={nameInputRef} required />
+          </div>
         </div>
-        <div className={css.control}>
-          <label htmlFor="name">Your name</label>
-          <input type="text" id="name" ref={nameInputRef} />
-        </div>
-      </div>
+      )}
       <div className={css.control}>
-        <label htmlFor="comment">Your comment</label>
-        <textarea id="comment" rows="3" ref={commentInputRef}></textarea>
+        <label htmlFor="comment">Comment</label>
+        <textarea
+          id="comment"
+          rows="1"
+          onFocus={handleCommentFocus}
+          ref={commentInputRef}
+          placeholder="Add a comment..."
+          required></textarea>
       </div>
-      <button>Submit</button>
+      {commentState && (
+        <>
+          <button
+            type="button"
+            className={css.cancel_btn}
+            onClick={handleCancelFocus}>
+            Cancel
+          </button>{" "}
+          <button type="submit">Submit</button>
+        </>
+      )}
 
-      {inputRes && (
+      {commentState && inputRes && (
         <ul className={css[inputRes[0] ? "error" : "success"]}>
           {inputRes[1]?.map((msg, i) => (
             <li key={i}>{msg}</li>
